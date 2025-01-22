478cc85fd9ab9d72caef38270189f7e718765675
Anggelo <anstalyn1999@gmail.com>
2025-01-21 23:52:08 -0500

    file-upload

---
+++ src/main/java/com/inventoryupdate/services_inventory_update/Repository/InventoryUpdateRepository.java
@@ -0,0 +1,7 @@
+package com.inventoryupdate.services_inventory_update.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.inventoryupdate.services_inventory_update.Models.Inventory;
+
+public interface InventoryUpdateRepository extends JpaRepository<Inventory, Integer>{
+    Inventory findByCodigoInventario(String codigoInventario);
+}
