1147a6a37b156c06a4de562da27835aa645fcb75
Anggelo <anstalyn1999@gmail.com>
2025-02-07 23:25:22 -0500

    feat: :fire: Deleting microservices files

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM openjdk:17-jdk-slim
-ARG JAR_FILE="target/app_microservices_update_inventory-0.0.1.jar"
-COPY ${JAR_FILE} app_iventory_update.jar
-EXPOSE 8082
-ENTRYPOINT ["java","-jar","app_iventory_update.jar"]
--- src/main/java/com/inventoryupdate/services_inventory_update/Repository/InventoryUpdateRepository.java
+++
@@ -1,7 +0,0 @@
-package com.inventoryupdate.services_inventory_update.Repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.inventoryupdate.services_inventory_update.Models.Inventory;
-
-public interface InventoryUpdateRepository extends JpaRepository<Inventory, Integer>{
-    Inventory findByInventoryCode(String inventoryCode);
-}
